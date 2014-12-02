1a121eb1952ef1b24e653e22da45640f60952b83
GoncaGuener <gonca-guener@hotmail.de>
2014-12-02 19:54:13 +0100

    Make a basic User model (including secure passwords)

--- config/application.rb
+++ config/application.rb
@@ -45,8 +45,14 @@ module SampleApp
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+
+    I18n.enforce_available_locales = true
+
+
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
+
+
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
